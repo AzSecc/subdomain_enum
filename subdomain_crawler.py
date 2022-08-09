ab4d57933c55498a595fa442d15ced816d853deb
r2sec <103225775+AzSecc@users.noreply.github.com>
2022-08-09 15:12:53 +0400

    Create subdomain_crawler.py

---
+++ subdomain_crawler.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import requests
+
+def request(url):
+    try:
+        return requests.get("http://" +url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+target_url = input("Enter Base URL (like example.com): ")
+
+print("Subdomain Crawling Results:\n")
+with open("C:\\Users\\User\\Desktop\\Python\\vbst_hck\\subdomains-wodlist.txt", "r") as wordlist_file:
+    for line in wordlist_file:
+        word = line.strip()
+        test_url = word + "." + target_url
+        response = request(test_url)
+        if response:
+            print("[+] Discovered Subdomain -->", test_url)
